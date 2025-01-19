de97c79929a429b775182b6fbfecb1d3ded4b8f3
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-01-19 13:51:06 +0600

    fix #1 fix the first issue on github.

--- app/src/main/java/com/kolu/learncomposenavigation/passingargumentstosecondscreen/screens/ScreenB.kt
+++ app/src/main/java/com/kolu/learncomposenavigation/passingargumentstosecondscreen/screens/ScreenB.kt
@@ -5,16 +5,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import com.kolu.learncomposenavigation.passingargumentstosecondscreen.navigation.PassArgTwoScreenNavScreens
@@ -24,8 +24,8 @@ fun ScreenB(
     modifier: Modifier = Modifier.fillMaxSize(),
     navController: NavController
 ) {
-    val backStackEntry by navController.currentBackStackEntryAsState()
-    val data = backStackEntry?.toRoute<PassArgTwoScreenNavScreens.ScreenB>()
+    val backStackEntry = navController.currentBackStackEntry
+    val data by remember { mutableStateOf(backStackEntry?.toRoute<PassArgTwoScreenNavScreens.ScreenB>()) }
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
